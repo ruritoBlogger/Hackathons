72791ea1f282fa1c592b4091a3cacaaa5d01c21f
ruritoblogger <rurito.blog@gmail.com>
2018-08-28 01:04:04 +0900

    add login moving

--- hackathons/config/routes.rb
+++ hackathons/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
+  post 'user/new' => "user#new"
   get 'home/top' => "home#top"
   get 'home/new' => "home#new"
   get 'home/login' => "home#login"
-
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
