b05ef03c20890e8585cd5d8541a551d13ef4d293
ruritoblogger <rurito.blog@gmail.com>
2018-08-28 00:32:24 +0900

    add new page

--- hackathons/config/routes.rb
+++ hackathons/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/top'
+
+  get 'home/top' => "home#top"
+  get 'home/new' => "home#new"
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
