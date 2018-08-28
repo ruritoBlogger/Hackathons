dde4c1041142b157cd4074b47fe86831ec1993e3
ruritoblogger <rurito.blog@gmail.com>
2018-08-29 03:25:03 +0900

    conect user info page and info

--- hackathons/app/controllers/form_controller.rb
+++ hackathons/app/controllers/form_controller.rb
@@ -10,12 +10,16 @@ class FormController < ApplicationController
 
   def create
     @form = Form.find_by(id: params[:id])
-    @post = Post.new(user_id: params[:id],  form_id: session[:user_id])
+    @post = Post.new(user_id: session[:user_id],  form_id: @form.id)
     @post.save
     redirect_to("/form/:id/info")
   end
 
   def info
+    @posts = Post.where(user_id: session[:user_id])
+  end
+
+  def delete
 
   end
 
