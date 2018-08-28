ab50eff72d50a15135eb56627532838025cdf2b4
ruritoblogger <rurito.blog@gmail.com>
2018-08-28 18:37:47 +0900

    make system about adding house info

--- hackathons/app/controllers/main_controller.rb
+++ hackathons/app/controllers/main_controller.rb
@@ -12,28 +12,27 @@ class MainController < ApplicationController
     @house = House.find_by(user_id: session[:user_id])
   end
 
+  def edit
+    @user = User.find_by(id: session[:user_id])
+    @house = House.find_by(user_id: session[:user_id])
+  end
+
 
   def update
     @user = User.find_by(id: session[:user_id])
     @house = House.find_by(user_id: session[:user_id])
-    if @house
-      @house.house_name = params[:house_name]
-      @house.detail = params[:detail]
-      @house.canVisit = params[:canVisit]
-      @house.caution = params[:caution]
-      @house.save
+    @house.user_id = session[:user_id]
+    @house.house_name = params[:house_name]
+    @house.detail = params[:detail]
+    @house.canVisit = params[:canVisit]
+    @house.caution = params[:caution]
+
+    if @house.save
       flash[:notice] = "edit sucsess!"
       redirect_to("/main/#{session[:user_id]}")
     else
-      @house = House.new(user_name: @user.name,
-                         house_name: params[:house_name],
-                         detail: params[:detail],
-                         canVisit: params[:canVisit],
-                         caution: params[:caution],
-                         user_id: @user.id)
-      @house.save
-      flash[:notice] = "edit sucsess"
-      redirect_to("/main/#{session[:user_id]}")
+      flash[:notice] = "failed"
+      render('main/update')
     end
   end
 
