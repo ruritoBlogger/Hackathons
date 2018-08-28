b6b5030bae9a2844554022f82b4df106c33f5e79
ruritoblogger <rurito.blog@gmail.com>
2018-08-28 23:36:13 +0900

    add area mode

--- hackathons/app/controllers/main_controller.rb
+++ hackathons/app/controllers/main_controller.rb
@@ -14,6 +14,7 @@ class MainController < ApplicationController
     else
       flash[:notice] = "please make your data"
       @house = House.new(user_id: session[:user_id])
+      @house.area = 2
       @house.save
       redirect_to("/main/#{session[:user_id]}/edit")
     end
@@ -36,7 +37,7 @@ class MainController < ApplicationController
 
     if @house.save
       flash[:notice] = "edit sucsess!"
-      redirect_to("/main/#{session[:user_id]}/serect")
+      redirect_to("/main/#{session[:user_id]}/select")
     else
       flash[:notice] = "failed"
       render('main/update')
@@ -47,11 +48,28 @@ class MainController < ApplicationController
   end
 
   def addTokyo
-
+    @user = User.find_by(id: session[:user_id])
+    @house = House.find_by(user_id: session[:id])
+      if @house.area == 1
+        redirect_to("main/#{session[:user_id]}")
+      else
+        @house.area = 1
+        @house.save
+        flash[:notice] = "area selected!"
+        redirect_to("/main/#{session[:user_id]}")
+      end
   end
 
   def addMinakusa
-
+    @user = User.find_by(id: session[:user_id])
+    @house = House.find_by(user_id: session[:user_id])
+      if @house.area == 2
+        redirect_to("/main/#{session[:user_id]}")
+      else
+        @house.save
+        flash[:notice] = "area selected!"
+        redirect_to("/main/#{session[:user_id]}")
+      end
   end
 
 end
