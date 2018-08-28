24b3857cb3acc0cb717c8e5725009331defa3355
ruritoblogger <rurito.blog@gmail.com>
2018-08-28 12:48:33 +0900

    add user_id to House table

--- hackathons/db/schema.rb
+++ hackathons/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_182801) do
+ActiveRecord::Schema.define(version: 2018_08_28_034101) do
 
   create_table "houses", force: :cascade do |t|
     t.string "user_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_182801) do
     t.text "caution"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
